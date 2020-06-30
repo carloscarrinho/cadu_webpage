f187824d66643a3df10e6b58106d286680ff48f4
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2020-06-29 21:10:40 -0300

    Finish project management without git flow

--- index.php
+++ index.php
@@ -3,10 +3,42 @@ ob_start();
 
 require __DIR__ . "/vendor/autoload.php";
 
+
 /**
  * BOOTSTRAP
  */
+use CoffeeCode\Router\Router;
 
+$route = new Router(url(), "@");
+
+
+/**
+ * WEB ROUTES
+ */
+$route->namespace("Source\App");
+$route->get("/", "Web@home");
+$route->get("/about", "Web@about");
+
+
+/**
+ * ERROR ROUTES
+ */
+$route->namespace("Source\App")->group("ops");
+$route->get("/{errcode}", "Web@error");
+
+
+/**
+ * 
+ */
+$route->dispatch();
+
+
+/**
+ * ERROR REDIRECT
+ */
+if($route->error()){
+    $route->redirect("/ops/{$route->error()}");
+}
 
 
 ob_end_flush();
---
+++ themes/light/home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home - Cadu</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">about</a></li>
+                <li><a href="#">jobs</a></li>
+                <li><a href="#">insights</a></li>
+                <li><a href="#">find.me</a></li>
+            </ul>
+        </nav>
+        <h1>Aqui vai a minha foto com um border radius</h1>
+    </header>
+    <main>
+        <h2>Aqui vão os 5 últimos posts do blog (insights)</h2>
+    </main>
+</body>
+</html>
